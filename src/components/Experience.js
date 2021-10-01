ed00f31a692e1a818c11752ff87f2852bd06e6c0
ashrestha1 <oscaaah@gmail.com>
2021-10-02 01:06:19 +0800

    first commit

---
+++ src/components/Experience.js
@@ -0,0 +1,116 @@
+import React, { Component } from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import Badge from "react-bootstrap/Badge";
+import islandSchoolIcon from '../experienceIcon/islandschool.png'
+import oriiIcon from '../experienceIcon/orii.png'
+import { ReactComponent as SchoolIcon } from './school.svg';
+import { ReactComponent as OrigamiIcon } from './engineer.svg';
+import './experience.styles.css';
+class Experience extends Component {
+  render() {
+    if (this.props.resumeExperience && this.props.resumeBasicInfo) {
+      var sectionName = this.props.resumeBasicInfo.section_name.experience;
+      var work = this.props.resumeExperience.map(function (work, i) {
+        const technologies = work.technologies;
+        const mainTechnologies = work.mainTech;
+
+        var mainTech = mainTechnologies.map((technology, i) => {
+          return (
+            <Badge pill className="main-badge mr-2 mb-2" key={i}>
+              {technology}
+            </Badge>
+          );
+        });
+        var tech = technologies.map((technology, i) => {
+          return (
+            <Badge pill className="experience-badge mr-2 mb-2" key={i}>
+              {technology}
+            </Badge>
+          );
+        });
+
+        var iconImage
+        var icons
+        switch (i) {
+          case 0:
+            iconImage = islandSchoolIcon;
+            icons = <SchoolIcon />;
+            break
+          case 1:
+            iconImage = oriiIcon;
+            icons = <OrigamiIcon />;
+            break
+          default:
+        }
+
+        return (
+
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date={work.years}
+            iconStyle={{
+              background: "#AE944F",
+              color: "#fff",
+              textAlign: "center",
+            }}
+            icon={icons}
+            key={i}
+          >
+            <div style={{ textAlign: "center", marginBottom: "10px" }}>
+              <img src={iconImage} className="img-resize" alt="myLogo" />
+            </div>
+
+            <h3
+              className="title"
+              style={{ textAlign: "center" }}
+            >
+              {work.title}
+            </h3>
+            <h4
+              className="subTitle"
+              style={{ textAlign: "center" }}
+            >
+              {work.company}
+            </h4>
+            <div style={{ textAlign: "center", marginTop: "15px" }}>{tech}</div>
+          </VerticalTimelineElement>
+        );
+      });
+    }
+
+    return (
+      <section id="resume" className="pb-5">
+        <div className="col-md-12 mx-auto">
+          <div className="col-md-12">
+            <h1 className="section-title" style={{ color: "black" }}>
+              <span className="text-black" style={{ textAlign: "center" }}>
+                {sectionName}
+              </span>
+            </h1>
+          </div>
+        </div>
+        <div className="col-md-8 mx-auto">
+          <VerticalTimeline>
+            {work}
+            <VerticalTimelineElement
+              iconStyle={{
+                background: "#AE944F",
+                color: "#fff",
+                textAlign: "center",
+              }}
+              icon={
+                <i className="fas fa-hourglass-start mx-auto experience-icon"></i>
+              }
+            />
+          </VerticalTimeline>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Experience;
