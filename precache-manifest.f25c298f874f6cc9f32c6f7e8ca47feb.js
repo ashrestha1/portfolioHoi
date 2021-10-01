e3b074c099d20ccad16ccd8d09004e382fb7b883
ashrestha1 <oscaaah@gmail.com>
2021-10-02 01:07:45 +0800

    Updates

---
+++ precache-manifest.f25c298f874f6cc9f32c6f7e8ca47feb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0366acc67acee686377a45756edf83d9",
+    "url": "/portfolioHoi/index.html"
+  },
+  {
+    "revision": "642e2b85586262797bb8",
+    "url": "/portfolioHoi/static/css/2.9497e657.chunk.css"
+  },
+  {
+    "revision": "a96e506463edada1b35f",
+    "url": "/portfolioHoi/static/css/main.7a9a1dcd.chunk.css"
+  },
+  {
+    "revision": "642e2b85586262797bb8",
+    "url": "/portfolioHoi/static/js/2.67cd2cd9.chunk.js"
+  },
+  {
+    "revision": "efc0bc89bcf702a3aa1127d8ad15fc2e",
+    "url": "/portfolioHoi/static/js/2.67cd2cd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a96e506463edada1b35f",
+    "url": "/portfolioHoi/static/js/main.052a1f2c.chunk.js"
+  },
+  {
+    "revision": "8ec7930fca601f9d0c76",
+    "url": "/portfolioHoi/static/js/runtime-main.07fe78e2.js"
+  },
+  {
+    "revision": "0d872a1e92176c051ebe86d2be24d1c1",
+    "url": "/portfolioHoi/static/media/engineer.0d872a1e.svg"
+  },
+  {
+    "revision": "c89529b02051461f4ea04658e77b8a24",
+    "url": "/portfolioHoi/static/media/orii.c89529b0.png"
+  },
+  {
+    "revision": "926763ec4203310c411cbc70daafd356",
+    "url": "/portfolioHoi/static/media/school.926763ec.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/portfolioHoi/precache-manifest.f25c298f874f6cc9f32c6f7e8ca47feb.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolioHoi/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
